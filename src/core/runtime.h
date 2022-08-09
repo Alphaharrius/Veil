ef8f2aea00cf49d1b9b414ddf1ca5d3c5adf4e56
Alphaharrius <47113671+Alphaharrius@users.noreply.github.com>
2022-08-09 17:34:58 +0800

    [creation] runtime as a singleton

---
+++ src/core/runtime.h
@@ -0,0 +1,23 @@
+#ifndef VEIL_RUNTIME_H
+#define VEIL_RUNTIME_H
+
+#include "core/heap.h"
+
+namespace veil {
+
+    class Runtime {
+    public:
+        static Runtime &get_instance();
+
+        Runtime(Runtime const&) = delete;
+        void operator=(Runtime const&) = delete;
+    private:
+        explicit Runtime();
+        ~Runtime();
+
+        veil::Heap heap;
+    };
+
+}
+
+#endif //VEIL_RUNTIME_H
