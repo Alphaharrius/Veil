eef2d2d079b1cc94a5847120dd583a306ffb7246
Alphaharrius <47113671+Alphaharrius@users.noreply.github.com>
2022-08-09 17:38:03 +0800

    [update] removed trivial class Allocation; renamed AllocationPool -> HeapSection; added class MemoryReference & ReferenceTable

--- src/core/heap.cpp
+++ src/core/heap.cpp
@@ -1,46 +1,25 @@
 #include "core/heap.h"
-#include "util/diagnostics.h"
 #include "util/natives.h"
 
-bool veil::Allocation::is_allocated() const {
-    return this->address != nullptr && this->size != 0;
-}
-
-veil::AllocationPool::AllocationPool(uint8 *allocation_address, uint32 allocation_size) {
-    not_null(allocation_address);
-    assert(allocation_size > 0);
-
-    this->allocation_address = allocation_address;
-    this->allocation_offset = allocation_address;
-    this->allocation_size = allocation_size;
-    this->allocation_ceiling = allocation_address + allocation_size;
-}
-
-void veil::AllocationPool::allocate(veil::Allocation *allocation) {
-    not_null(allocation);
-
-    if (allocation->size == 0 || this->allocation_offset + allocation->size < this->allocation_ceiling) {
-        return;
+uint8 *veil::HeapSection::allocate(uint32 request_size) {
+    if (request_size == 0 || this->allocation_offset + request_size >= this->allocation_ceiling + 1) {
+        return nullptr;
     }
-    allocation->address = this->allocation_offset;
-    this->allocation_offset += allocation->size;
+    uint8 *allocated_address = this->allocation_offset;
+    memset((void *) allocated_address, 0, request_size);
+    this->allocation_offset += request_size;
+    return allocated_address;
 }
 
-void veil::AllocationPool::reuse(veil::AllocationPool reset_info) {
+void veil::HeapSection::reuse(veil::HeapSection reset_info) {
     this->allocation_address = reset_info.allocation_address;
     this->allocation_offset = reset_info.allocation_offset;
     this->allocation_ceiling = reset_info.allocation_ceiling;
     this->allocation_size = reset_info.allocation_size;
 }
 
-void veil::AllocationPool::reset() {
+void veil::HeapSection::reset() {
     this->allocation_offset = this->allocation_address;
-}
-
-veil::HeapCreationInfo::HeapCreationInfo(uint64 memory_size, uint32 internal_allocation_pool_size) {
-    this->memory_size = memory_size;
-    this->internal_allocation_pool_size = internal_allocation_pool_size;
-    this->error = HeapCreationInfo::Error::NONE;
 }
 
 veil::Heap::Heap(HeapCreationInfo &creation_info) {
@@ -48,9 +27,9 @@ veil::Heap::Heap(HeapCreationInfo &creation_info) {
         creation_info.error = HeapCreationInfo::Error::INVALID_MEMORY_SIZE;
         return;
     }
-    if (creation_info.internal_allocation_pool_size == 0 ||
-        creation_info.memory_size % creation_info.internal_allocation_pool_size != 0) {
-        creation_info.error = HeapCreationInfo::Error::INVALID_INTERNAL_ALLOCATION_POOL_SIZE;
+    if (creation_info.section_size == 0 ||
+        creation_info.memory_size % creation_info.section_size != 0) {
+        creation_info.error = HeapCreationInfo::Error::INVALID_INTERNAL_SECTION_SIZE;
         return;
     }
     void *allocated_address = veil::natives::virtual_allocate(creation_info.memory_size,
@@ -63,18 +42,81 @@ veil::Heap::Heap(HeapCreationInfo &creation_info) {
     this->memory_offset = this->memory_address;
     this->memory_ceiling = this->memory_address + creation_info.memory_size;
     this->memory_size = creation_info.memory_size;
-    this->internal_allocation_pool_size = creation_info.internal_allocation_pool_size;
+    this->section_size = creation_info.section_size;
 }
 
 veil::Heap::~Heap() {
-
+    if (this->memory_address != nullptr) {
+        natives::OperationStatus operation_status;
+        natives::virtual_free(this->memory_address, this->memory_size, operation_status);
+    }
 }
 
-veil::AllocationPool *veil::Heap::allocate_internal_allocation_pool() {
+veil::HeapSection *veil::Heap::allocate_section() {
+    if (this->memory_offset + this->section_size < this->memory_ceiling + 1) {
+        auto *internal_pool = new HeapSection(this->memory_offset,
+                                              this->section_size);
+        this->memory_offset += this->section_size;
+        return internal_pool;
+    }
     return nullptr;
 }
 
-veil::AllocationPool *veil::Heap::allocate_external_allocation_pool(uint64 request_size,
-                                                                    natives::OperationStatus &allocation_status) {
-    return nullptr;
+veil::concurrent::Synchronizable &veil::Heap::get_allocation_lock() {
+    return this->allocation_lock;
+}
+
+veil::MemoryReference::MemoryReference(uint8 *address) {
+    this->colored_reference = 0ULL;
+    this->update_address(address);
+}
+
+uint64 veil::MemoryReference::get_reference() const {
+    return this->colored_reference;
+}
+
+uint8 *veil::MemoryReference::get_address() const {
+    return (uint8 *) (this->colored_reference & MemoryReference::ADDRESS_BIT_MASK);
+}
+
+void veil::MemoryReference::update_address(const uint8 *address) {
+    uint64 masked_reference = ((uint64) address) & MemoryReference::ADDRESS_BIT_MASK;
+    this->colored_reference &= ~MemoryReference::ADDRESS_BIT_MASK;
+    this->colored_reference |= masked_reference;
+}
+
+void veil::MemoryReference::set_color(uint8 position, bool value) {
+    uint64 mask = (1ULL << (MemoryReference::ADDRESS_BIT_COUNT + position));
+    if (value) {
+        this->colored_reference |= mask;
+    } else {
+        this->colored_reference &= ~mask;
+    }
+}
+
+bool veil::MemoryReference::get_color(uint8 position) const {
+    uint64 mask = (1ULL << (MemoryReference::ADDRESS_BIT_COUNT + position));
+    return this->colored_reference & mask;
+}
+
+uint16 *veil::MemoryReference::color_bits() {
+    return ((uint16 *) &this->colored_reference) + 3;
+}
+
+veil::MemoryReference *veil::ReferenceTable::get_reference() {
+    return (veil::MemoryReference *) this->allocate(sizeof(veil::MemoryReference));
+}
+
+veil::MemoryReference *veil::ReferenceTable::reuse_reference(uint32 memory_size) {
+    veil::MemoryReference *reusable = nullptr;
+    for (auto *current = (veil::MemoryReference *) this->allocation_address;
+         current < (veil::MemoryReference *) this->allocation_offset; current++) {
+        // todo >> this condition should includes a check of whether the referenced memory size matches.
+        // todo >> the requested memory size should be at least 80% of the referenced memory size.
+        if (current->get_color(veil::MemoryReference::FREED_BIT_POSITION)) {
+            reusable = current;
+            break;
+        }
+    }
+    return reusable;
 }
--- src/core/heap.h
+++ src/core/heap.h
@@ -8,23 +8,24 @@
 
 namespace veil {
 
-    struct Allocation {
-        [[nodiscard]] bool is_allocated() const;
-
-        uint8 *address = nullptr;
-        uint32 size = 0;
-    };
-
-    class AllocationPool:
-            public veil::util::ChainableResource<AllocationPool>,
-            public veil::util::ReusableResource<AllocationPool>,
-            private veil::concurrent::Synchronizable {
+    class HeapSection :
+            public util::ChainableResource<HeapSection>,
+            public util::ReusableResource<HeapSection>,
+            private concurrent::Synchronizable {
     public:
-        AllocationPool(uint8 *allocation_address, uint32 allocation_size);
-        void allocate(Allocation *allocation);
+        HeapSection(uint8 *allocation_address, uint32 allocation_size) :
+                allocation_address(allocation_address),
+                allocation_offset(allocation_address),
+                allocation_size(allocation_size),
+                allocation_ceiling(allocation_address + allocation_size) {}
+
+        uint8 *allocate(uint32 request_size);
+
         void reset() override;
-        void reuse(AllocationPool reset_info) override;
-    private:
+
+        void reuse(HeapSection reset_info) override;
+
+    protected:
         uint8 *allocation_address;
         uint8 *allocation_offset;
         uint8 *allocation_ceiling;
@@ -35,14 +36,17 @@ namespace veil {
         enum Error {
             NONE,
             INVALID_MEMORY_SIZE,
-            INVALID_INTERNAL_ALLOCATION_POOL_SIZE,
+            INVALID_INTERNAL_SECTION_SIZE,
             ALLOCATION_ERROR
         };
 
-        HeapCreationInfo(uint64 memory_size, uint32 internal_allocation_pool_size);
+        HeapCreationInfo(uint64 memory_size, uint32 section_size) :
+                memory_size(memory_size),
+                section_size(section_size),
+                error(HeapCreationInfo::Error::NONE) {}
 
         uint64 memory_size;
-        uint32 internal_allocation_pool_size;
+        uint32 section_size;
         HeapCreationInfo::Error error;
         natives::OperationStatus allocation_status {};
     };
@@ -50,16 +54,57 @@ namespace veil {
     class Heap {
     public:
         explicit Heap(HeapCreationInfo &creation_info);
+
         ~Heap();
-        AllocationPool *allocate_internal_allocation_pool();
-        AllocationPool *allocate_external_allocation_pool(uint64 request_size,
-                                                          natives::OperationStatus &allocation_status);
+
+        HeapSection *allocate_section();
+
+        concurrent::Synchronizable &get_allocation_lock();
+
     private:
+        concurrent::Synchronizable allocation_lock;
+
         uint8 *memory_address;
         uint8 *memory_offset;
         uint8 *memory_ceiling;
         uint64 memory_size;
-        uint32 internal_allocation_pool_size;
+        uint32 section_size;
+    };
+
+    class MemoryReference : private concurrent::Synchronizable {
+    public:
+        static const uint8 ADDRESS_BIT_COUNT = 48;
+        static const uint64 ADDRESS_BIT_MASK = ~((~0ULL) << ADDRESS_BIT_COUNT);
+
+        static const uint8 FREED_BIT_POSITION = 15;
+
+        explicit MemoryReference(uint8 *address);
+
+        [[nodiscard]] uint64 get_reference() const;
+
+        [[nodiscard]] uint8 *get_address() const;
+
+        void update_address(const uint8 *address);
+
+        void set_color(uint8 position, bool value);
+
+        [[nodiscard]] bool get_color(uint8 position) const;
+
+        uint16 *color_bits();
+
+    private:
+        uint64 colored_reference;
+    };
+
+    class ReferenceTable : protected HeapSection {
+    public:
+        MemoryReference *get_reference();
+
+        MemoryReference *reuse_reference(uint32 memory_size);
+
+    private:
+        ReferenceTable(uint8 *table_address, uint32 table_size) :
+                HeapSection(table_address, table_size) {}
     };
 
 }
