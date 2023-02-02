db7e5b71b7c9d5090dbb2561c6293bc2e69121b8
Alphaharrius <alphaharrius@gmail.com>
2023-02-03 02:15:37 +0800

    Added GNU license header for all existing files.

--- src/memory/memory.cpp
+++ src/memory/memory.cpp
@@ -1,5 +1,20 @@
 #include "memory/memory.h"
 
+/// This file is part of the Veil distribution (https://github.com/Alphaharrius/Veil).
+/// Copyright (c) 2023 Alphaharrius.
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, version 3.
+///
+/// This program is distributed in the hope that it will be useful, but
+/// WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+/// General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 namespace veil::memory {
 
     Pointer *Allocator::allocate(AllocateRequest &request) {
