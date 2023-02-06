0a8b2a0fc0efa09bf74df4c42c647373a3de2cbf
Alphaharrius <alphaharrius@gmail.com>
2023-02-07 00:56:09 +0800

    `not_null` preprocessor is never used.

--- src/diagnostics.h
+++ src/diagnostics.h
@@ -16,12 +16,9 @@
 /// You should have received a copy of the GNU General Public License
 /// along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-#include <cassert>
 #include <string>
 
 #include "typedefs.h"
-
-#define not_null(pointer) assert(pointer != nullptr)
 
 namespace veil::util {
 
