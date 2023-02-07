7c2e7cc932848cda7bfde261e79ade22903e5af5
Alphaharrius <alphaharrius@gmail.com>
2023-02-07 19:27:01 +0800

    Restructured native method calls.

--- src/natives.cpp
+++ src/natives.cpp
@@ -4,33 +4,61 @@
 
 #include <windows.h>
 
-#elif defined(__linux__) || defined(__linux) || defined(linux)
+#elif defined(__linux__) || defined(__linux) || defined(linux) || defined(__CYGWIN__)
+
 #include <sys/mman.h>
+#include <unistd.h>
+
 #endif
 
 namespace veil::natives {
 
     uint32 get_page_size() {
 #       if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-        _SYSTEM_INFO system_info {};
+        _SYSTEM_INFO system_info{};
         GetSystemInfo(&system_info);
         return static_cast<uint32>(system_info.dwPageSize);
-#       elif defined(__linux__) || defined(__linux) || defined(linux)
-        // TODO: Implement Linux usage.
+#       elif defined(__linux__) || defined(__linux) || defined(linux) || defined(__CYGWIN__)
+        return (uint32) getpagesize();
 #       endif
     }
 
-    uint8 *malloc(uint64 size) {
+    Mmap::Mmap(
+            void *address,
+            uint64 size,
+            bool readwrite,
+            bool reserve) : address(address), size(size), readwrite(readwrite), reserve(reserve) {}
+
+    bool Mmap::access() {
         void *allocated_address = nullptr;
 #       if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
         allocated_address = VirtualAlloc((LPVOID) nullptr,
                                          (SIZE_T) size,
-                                         MEM_RESERVE | MEM_COMMIT,
-                                         PAGE_READWRITE);
-#       elif defined(__linux__) || defined(__linux) || defined(linux)
-        // TODO: Implement Linux usage.
+                                         (reserve ? MEM_RESERVE | MEM_COMMIT : 0),
+                                         (readwrite ? PAGE_READWRITE : 0));
+        return allocated_address != nullptr;
+#       elif defined(__linux__) || defined(__linux) || defined(linux) || defined(__CYGWIN__)
+        allocated_address = mmap(nullptr,
+                                 size,
+                                 (readwrite ? PROT_READ | PROT_WRITE : 0),
+                                 MAP_PRIVATE | (reserve ? 0 : MAP_NORESERVE),
+                                 -1, 0);
+        this->result = static_cast<uint8 *>(allocated_address);
+        return allocated_address != MAP_FAILED;
 #       endif
-        return static_cast<uint8 *>(allocated_address);
+    }
+
+    uint32 Mmap::error() {
+#       if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+        switch((uint32) GetLastError()) {
+            case ERROR_NOT_ENOUGH_MEMORY: return ERR_NOMEM;
+        }
+#       elif defined(__linux__) || defined(__linux) || defined(linux) || defined(__CYGWIN__)
+        switch ((uint32) errno) {
+            case ENOMEM: return ERR_NOMEM;
+        }
+#       endif
+        return 0;
     }
 
 }
--- src/natives.h
+++ src/natives.h
@@ -8,7 +8,41 @@ namespace veil::natives {
 
     uint32 get_page_size();
 
-    uint8 *malloc(uint64 size);
+    uint8 *virtual_alloc(uint64 size);
+
+    template <typename T>
+    class NativeAccess {
+    public:
+        virtual bool access() = 0;
+
+        virtual uint32 error() = 0;
+
+        T get_result();
+
+    protected:
+        T result;
+    };
+
+    template<typename T>
+    T NativeAccess<T>::get_result() {
+        return this->result;
+    }
+
+    class Mmap : public NativeAccess<uint8 *> {
+    public:
+        static const uint32 ERR_NOMEM = 1;
+
+        Mmap(void *address, uint64 size, bool readwrite, bool reserve);
+
+        bool access() override;
+
+        uint32 error() override;
+
+    private:
+        void *address;
+        uint64 size;
+        bool readwrite, reserve;
+    };
 
 }
 
