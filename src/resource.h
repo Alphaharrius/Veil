9535de3b34a3e0d9f50ab4ebf8511e377a447290
Alphaharrius <alphaharrius@gmail.com>
2023-02-10 01:50:13 +0800

    Using new type of resource holders.

--- src/resource.h
+++ src/resource.h
@@ -4,23 +4,55 @@
 namespace veil::util {
 
     template<typename T>
-    class Resource {
+    class Storage {
     public:
-        Resource();
+        static const uint32 INITIAL_BUFFER_LEN = 64;
 
-        T *get_prev();
+        explicit Storage();
 
-        T *get_next();
+        T get(uint32 index);
 
-        void pop(Resource<T> *elem);
+        T pop();
 
-        void insert(Resource<T> *group);
+        void push(T el);
 
-    private:
-        T *prev;
-        T *next;
+        uint32 get_top_index();
+
+    protected:
+        T *buffer;
+        uint32 buffer_len;
+        uint32 top_index;
     };
 
+    template<typename T>
+    Storage<T>::Storage() : buffer(new T[INITIAL_BUFFER_LEN]), buffer_len(INITIAL_BUFFER_LEN), top_index(0) {}
+
+    template<typename T>
+    T Storage<T>::get(uint32 index) {
+        return this->buffer[index];
+    }
+
+
+    template<typename T>
+    T Storage<T>::pop() {
+        return buffer[this->top_index--];
+    }
+
+    template<typename T>
+    void Storage<T>::push(T el) {
+        if (this->top_index == this->buffer_len) {
+            auto *new_buffer = new T[this->buffer_len * 1.5f];
+            memcpy(new_buffer, this->buffer, this->top_index);
+            delete[] this->buffer;
+            this->buffer = new_buffer;
+        }
+        this->buffer[this->top_index++] = el;
+    }
+
+    template<typename T>
+    uint32 Storage<T>::get_top_index() {
+        return this->top_index;
+    }
 }
 
 #endif //VEIL_RESOURCE_H
