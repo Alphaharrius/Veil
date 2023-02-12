18e3d0724c76385d9e9b1cd33182e3b5d6d8f539
Alphaharrius <alphaharrius@gmail.com>
2023-02-13 01:42:43 +0800

    Renaming an attribute.

--- src/resource.h
+++ src/resource.h
@@ -21,11 +21,11 @@ namespace veil::util {
     protected:
         T *buffer;
         uint32 buffer_len;
-        uint32 top_index;
+        uint32 buffered_count;
     };
 
     template<typename T>
-    Storage<T>::Storage() : buffer(new T[INITIAL_BUFFER_LEN]), buffer_len(INITIAL_BUFFER_LEN), top_index(0) {}
+    Storage<T>::Storage() : buffer(new T[INITIAL_BUFFER_LEN]), buffer_len(INITIAL_BUFFER_LEN), buffered_count(0) {}
 
     template<typename T>
     T Storage<T>::get(uint32 index) {
@@ -35,23 +35,23 @@ namespace veil::util {
 
     template<typename T>
     T Storage<T>::pop() {
-        return buffer[this->top_index--];
+        return buffer[this->buffered_count--];
     }
 
     template<typename T>
     void Storage<T>::push(T el) {
-        if (this->top_index == this->buffer_len) {
+        if (this->buffered_count == this->buffer_len) {
             auto *new_buffer = new T[this->buffer_len * 1.5f];
-            memcpy(new_buffer, this->buffer, this->top_index);
+            memcpy(new_buffer, this->buffer, this->buffered_count);
             delete[] this->buffer;
             this->buffer = new_buffer;
         }
-        this->buffer[this->top_index++] = el;
+        this->buffer[this->buffered_count++] = el;
     }
 
     template<typename T>
     uint32 Storage<T>::get_top_index() {
-        return this->top_index;
+        return this->buffered_count;
     }
 }
 
