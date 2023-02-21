0c81eed4e765b457ef3ecf615b5313a45b22bbe6
Alphaharrius <alphaharrius@gmail.com>
2023-02-21 23:40:00 +0800

    Removal of trivial includes.

--- fabric/src/memory/os.cpp
+++ fabric/src/memory/os.cpp
@@ -13,8 +13,6 @@
 /// You should have received a copy of the GNU General Public License
 /// along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-#include <string>
-
 #include "src/memory/os.hpp"
 #include "src/vm/os.hpp"
 #include "src/vm/errors.hpp"
