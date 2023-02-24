f04c297fc4811b5db551b3fd8dd5176cac82b8e0
Alphaharrius <alphaharrius@gmail.com>
2023-02-25 02:51:15 +0800

    Updated test cases.

--- fabric/src/threading/tests/atomic_test.cpp
+++ fabric/src/threading/tests/atomic_test.cpp
@@ -13,18 +13,23 @@ int main() {
     auto *obj = new Object();
 
     std::cout << "Initial address: 0x" << std::hex << reinterpret_cast<uint64>(obj) << std::endl;
+    std::cout << "Expected: 0x" << std::hex << reinterpret_cast<uint64>(obj) << std::endl << std::endl;
 
     atomic_ptr<Object> a_ptr(obj);
 
     std::cout << "Address loaded: 0x" << std::hex << reinterpret_cast<uint64>(a_ptr.load()) << std::endl;
+    std::cout << "Expected: 0x" << std::hex << reinterpret_cast<uint64>(obj) << std::endl << std::endl;
 
     std::cout << "Address compare-exchanged: 0x" << std::hex
               << reinterpret_cast<uint64>(a_ptr.compare_exchange(nullptr, obj)) << std::endl;
+    std::cout << "Expected: 0x" << std::hex << reinterpret_cast<uint64>(obj) << std::endl << std::endl;
 
     std::cout << "Address exchanged: 0x" << std::hex << reinterpret_cast<uint64>(a_ptr.exchange(nullptr)) << std::endl;
+    std::cout << "Expected: 0x" << std::hex << reinterpret_cast<uint64>(obj) << std::endl << std::endl;
 
     std::cout << "Address compare-exchanged: 0x" << std::hex
               << reinterpret_cast<uint64>(a_ptr.compare_exchange(nullptr, obj)) << std::endl;
+    std::cout << "Expected: 0x" << std::hex << reinterpret_cast<uint64>(nullptr) << std::endl << std::endl;
 
     delete obj;
 }
