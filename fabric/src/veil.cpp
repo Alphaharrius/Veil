9b9b79aee74f0b68d19b494e85d6fd558c96376f
Alphaharrius <alphaharrius@gmail.com>
2023-03-01 03:01:15 +0800

    Added main.

---
+++ fabric/src/veil.cpp
@@ -0,0 +1,10 @@
+#include "src/memory/management.hpp"
+#include "src/threading/management.hpp"
+#include "src/core/runtime.hpp"
+
+using namespace veil;
+
+int main(int argc, char *argv[]) {
+    Runtime runtime;
+    threading::Management management;
+}
